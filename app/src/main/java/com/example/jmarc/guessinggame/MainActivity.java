64551319cdcff2fffc7185e03e9233f781e9f196
jmarc <jared.marcolongo@my.jcu.edu.au>
2017-03-13 23:01:52 +1000

    working on guessing game

--- app/src/main/java/com/example/jmarc/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/jmarc/guessinggame/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.jmarc.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView statusText;
+    private EditText guessField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statusText = (TextView) findViewById(R.id.statusText);
+        guessField = (EditText) findViewById(R.id.guessField);
+
+        
     }
 }
