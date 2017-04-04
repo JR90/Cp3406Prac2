4d26a72762da8f1c0531f714a148a64aeeecd22b
jc321013 <jared.marcolongo@my.jcu.edu.au>
2017-04-04 10:48:05 +1000

    rgffdg

--- app/src/main/java/com/example/jmarc/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/jmarc/guessinggame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.jmarc.guessinggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,14 +12,47 @@ public class MainActivity extends AppCompatActivity {
     private TextView statusText;
     private EditText guessField;
 
+    private final static  int secretNumber = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         statusText = (TextView) findViewById(R.id.statusText);
         guessField = (EditText) findViewById(R.id.guessField);
 
-        
+        statusText.setText("Hello world");
+
+        guessField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence string, int start, int before, int count) {
+//                method is when user types something in this method changes
+
+                int value = Integer.parseInt(string.toString());
+
+                if (value == secretNumber) {
+                    statusText.setText("you won");
+                }
+
+                statusText.setText(string);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
     }
 }
+
+
