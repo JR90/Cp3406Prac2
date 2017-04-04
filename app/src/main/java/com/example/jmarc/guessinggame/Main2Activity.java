72822d129fbd2ab92df76ac1d8c7b06689d35320
jc321013 <jared.marcolongo@my.jcu.edu.au>
2017-04-04 11:20:11 +1000

    rgffdg

---
+++ app/src/main/java/com/example/jmarc/guessinggame/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.example.jmarc.guessinggame;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jc321013 on 4/04/2017.
+ */
+
+public class Main2Activity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
